799741678fb4a4e675c847a93bd6fcf578f8d5a2
Felix Lutze <felix.lutze@outlook.de>
2021-03-30 15:26:26 +0000

    Save Output

--- app.py
+++ app.py
@@ -63,6 +63,7 @@ def predict():
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
+    LOG.info(f"logging_output_prediction: \n{inference_payload}")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
