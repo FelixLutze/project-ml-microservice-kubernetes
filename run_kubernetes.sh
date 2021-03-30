f3994cae0a0a0ab7c1446dbd128e9b4822dcd209
Felix Lutze <felix.lutze@outlook.de>
2021-03-30 16:19:59 +0000

    Deploy with Kubernetes and Save Output Logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=machine-learning-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run $dockerpath --image=felixlutze/machine-learning-api:latest --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward machine-learning-api 8000:80
