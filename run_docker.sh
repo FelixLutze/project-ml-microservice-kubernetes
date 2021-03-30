f98342774be748d2f462f7782d63df33f87bb65e
Felix Lutze <felix.lutze@outlook.de>
2021-03-30 16:05:50 +0000

    change name of container (kubectl doesnt support Underscores)

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=machine_learning_api .
+docker build --tag=machine-learning-api .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 machine_learning_api
+docker run -p 8000:80 machine-learning-api
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,14 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath=machine_learning_api
+dockerpath=machine-learning-api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 docker login --username felixlutze
-docker tag machine_learning_api felixlutze/machine_learning_api:latest
+docker tag machine-learning-api felixlutze/machine-learning-api:latest
 
 # Step 3:
 # Push image to a docker repository
-docker push felixlutze/machine_learning_api:latest
+docker push felixlutze/machine-learning-api:latest
